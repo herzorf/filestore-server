dca6f32263d3e239ba3a55b36a9ebb789e3e80ab
herzorf <1446450047@qq.com>
2023-01-09 22:51:05 +0800

    返回html文件里的内容

--- handler/handler.go
+++ handler/handler.go
@@ -3,12 +3,21 @@ package handler
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 // UploadHandler 处理上传文件
 func UploadHandler(writer http.ResponseWriter, request *http.Request) {
 	if request.Method == "GET" {
-		fmt.Println(request.Method)
+		file, err := os.ReadFile("./static/view/index.html")
+		if err != nil {
+			fmt.Printf("文件读取错误 %s", err.Error())
+		}
+		write, err := writer.Write(file)
+		if err != nil {
+			fmt.Println("文件写入错误", err)
+		}
+		fmt.Println(write)
 	} else if request.Method == "POST" {
 		fmt.Println(request.Method)
 
