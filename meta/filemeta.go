76ab0963b27ebd6b47e7c30cddc2b8d076dc6667
herzorf <1446450047@qq.com>
2023-01-10 22:06:29 +0800

    添加文件结构和增改查操作

---
+++ meta/filemeta.go
@@ -0,0 +1,26 @@
+package meta
+
+// FileMeta 文件元信息
+type FileMeta struct {
+	FileSha1 string
+	FileName string
+	FileSize int64
+	Location string
+	UploadAt string
+}
+
+var FileMetas map[string]FileMeta
+
+func init() {
+	FileMetas = make(map[string]FileMeta)
+}
+
+// UploadFileMeta 新增和更新fmeta
+func UploadFileMeta(fmeta FileMeta) {
+	FileMetas[fmeta.FileSha1] = fmeta
+}
+
+// GetFileMeta 获取FileMetas里的元信息对象
+func GetFileMeta(filesha1 string) FileMeta {
+	return FileMetas[filesha1]
+}
