929a5efb95b470fead453e4e8fa8a48aaf18c548
herzorf <1446450047@qq.com>
2023-01-15 15:09:16 +0800

    用户注册接口完成

---
+++ handler/user.go
@@ -0,0 +1,56 @@
+package handler
+
+import (
+	"fmt"
+	"github.com/herzorf/filestroe-server/db"
+	"github.com/herzorf/filestroe-server/util"
+	"net/http"
+	"os"
+)
+
+const pwdSalt = "*#890"
+
+func SignUpHandler(write http.ResponseWriter, request *http.Request) {
+	if request.Method == "GET" {
+		file, err := os.ReadFile("./static/view/signup.html")
+		if err != nil {
+			fmt.Println("文件读取错误", err)
+			write.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		_, err = write.Write(file)
+		if err != nil {
+			fmt.Println("write err", err)
+			write.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	} else if request.Method == "POST" {
+		err := request.ParseForm()
+		if err != nil {
+			fmt.Println("request.ParseForm err", err)
+			write.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		username := request.Form.Get("username")
+		password := request.Form.Get("password")
+		if len(password) < 5 || len(username) < 3 {
+			_, err := write.Write([]byte("用户名，密码格式不对"))
+			if err != nil {
+				fmt.Println("write err", err)
+				write.WriteHeader(http.StatusInternalServerError)
+			}
+			return
+		}
+		encPassword := util.Sha1([]byte(password + pwdSalt))
+		suc := db.UserSignUp(username, encPassword)
+		if !suc {
+			write.WriteHeader(http.StatusInternalServerError)
+			return
+		} else {
+			_, err := write.Write([]byte("success sign in "))
+			if err != nil {
+				fmt.Println("write err ", err)
+			}
+		}
+	}
+}
--- main.go
+++ main.go
@@ -15,6 +15,7 @@ func main() {
 	http.HandleFunc("/file/download", handler.DownloadHandler)
 	http.HandleFunc("/file/update", handler.FileMetaUpdateHandler)
 	http.HandleFunc("/file/delete", handler.FileDeleteHandler)
+	http.HandleFunc("/user/signup", handler.SignUpHandler)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		fmt.Printf("server start err %s\n", err.Error())
