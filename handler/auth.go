9d828f0f1999e3b07a9ee5423f71aaafd7538e0b
herzorf <1446450047@qq.com>
2023-01-26 20:02:09 +0800

    重写拦截器

--- handler/auth.go
+++ handler/auth.go
@@ -1,37 +1,22 @@
 package handler
 
 import (
-	"bytes"
-	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"github.com/herzorf/filestroe-server/response"
-	"io"
-	"log"
 	"net/http"
 )
-
-type UserInfo struct {
-	Username string `json:"username"`
-	Token    string `json:"token"`
-}
 
 // HTTPIntercepter 请求拦截器
 func HTTPIntercepter() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		var userInfo UserInfo
-		requestBody, err := c.GetRawData()
-		if err != nil {
-			log.Println(err.Error())
-		}
-		err = json.Unmarshal(requestBody, &userInfo)
-		if err != nil {
-			log.Println("json Unmarshal err", err)
-		}
-		c.Request.Body = io.NopCloser(bytes.NewBuffer(requestBody)) // 关键点
-		if len(userInfo.Username) < 6 || !IsTokenValid(userInfo.Username, userInfo.Token) {
-			response.Response(c, http.StatusForbidden, 403, "token错误或用户名格式不对", nil)
+		username := c.GetHeader("username")
+		token := c.GetHeader("token")
+		if len(username) < 6 || !IsTokenValid(username, token) {
+			c.Abort()
+			response.Response(c, http.StatusForbidden, 403, username+token, nil)
 			return
+		} else {
+			c.Next()
 		}
-		c.Next()
 	}
 }
