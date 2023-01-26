00a35ac7f53c9145482174029438006f06a250e5
herzorf <1446450047@qq.com>
2023-01-26 21:40:10 +0800

    删除无用代码

--- main.go
+++ main.go
@@ -12,20 +12,14 @@ func main() {
 	if err != nil {
 		fmt.Println("gin run err", err)
 	}
-	//http.HandleFunc("/api/file/upload", handler.UploadHandler) //do
-
-	////http.HandleFunc("/api/file/upload/suc", handler.UploadSucHandler)
 	//http.HandleFunc("/api/file/meta", handler.GetFileMetaHandler)
 	//http.HandleFunc("/api/file/download", handler.DownloadHandler)
 	//http.HandleFunc("/api/file/update", handler.FileMetaUpdateHandler)
 	//http.HandleFunc("/api/file/delete", handler.FileDeleteHandler)
+
 	//http.HandleFunc("/api/file/fastUpload", handler.HTTPIntercepter(handler.TryFastUploadHandler))
 	//http.HandleFunc("/api/file/mpupload/init", handler.HTTPIntercepter(handler.InitialMultipartUploadHandler))
 	//http.HandleFunc("/api/file/mpupload/uppart", handler.HTTPIntercepter(handler.UploadPartHandler))
 	//http.HandleFunc("/api/file/mpupload/complete", handler.HTTPIntercepter(handler.CompleteUploadHandler))
-	//
-	//err := http.ListenAndServe(":8080", nil)
-	//if err != nil {
-	//	fmt.Printf("server start err %s\n", err.Error())
-	//}
+
 }
